1fd211544fbd252a22afd43886905508e923f7f2
henrique-hf <henriquefonseca.hf@gmail.com>
2023-05-30 23:56:25 -0300

    Add files via upload

---
+++ src/main/java/com/example/CoffeeShop/model/NotaFiscal.java
@@ -0,0 +1,22 @@
+package Model;
+
+public class NotaFiscal {
+    int nfId;
+    int nfOficialNumber;
+
+    public int getNfId() {
+        return nfId;
+    }
+
+    public void setNfId(int nfId) {
+        this.nfId = nfId;
+    }
+
+    public int getNfOficialNumber() {
+        return nfOficialNumber;
+    }
+
+    public void setNfOficialNumber(int nfOficialNumber) {
+        this.nfOficialNumber = nfOficialNumber;
+    }
+}
