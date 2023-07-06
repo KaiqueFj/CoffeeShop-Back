bae445cbc576f062925ccfbbb4dd4c3a9e5fbed3
KaiqueFj <kaiquelferraz@hotmail.com>
2023-07-06 05:43:01 -0300

    feat: added Response entity status to the Client Controller

--- src/main/java/com/example/CoffeeShop/controller/ClienteController.java
+++ src/main/java/com/example/CoffeeShop/controller/ClienteController.java
@@ -1,9 +1,7 @@
 package com.example.CoffeeShop.controller;
 
-import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
