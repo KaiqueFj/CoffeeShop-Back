53207392949d80bc7ead75e7cbdea2902fcab587
KaiqueFj <kaiquelferraz@hotmail.com>
2023-07-06 05:13:40 -0300

    feat: added the remove method for order entity

--- src/main/java/com/example/CoffeeShop/model/pedido/Pedido.java
+++ src/main/java/com/example/CoffeeShop/model/pedido/Pedido.java
@@ -37,11 +37,11 @@ public class Pedido {
     String vl_pedido;
 
     // Relantionships
-    @ManyToOne(cascade = { CascadeType.MERGE }, fetch = FetchType.EAGER)
+    @ManyToOne(cascade = { CascadeType.MERGE, CascadeType.REMOVE }, fetch = FetchType.EAGER)
     @JoinColumn(name = "T_cliente_id_cliente", referencedColumnName = "id_cliente")
     private Cliente T_cliente_id_cliente;
 
-    @OneToOne(cascade = { CascadeType.MERGE }, fetch = FetchType.EAGER)
+    @OneToOne(cascade = { CascadeType.MERGE, CascadeType.REMOVE }, fetch = FetchType.EAGER)
     @JoinColumn(name = "T_notafiscal_id_notafiscal", referencedColumnName = "id_Notafiscal")
     private NotaFiscal T_notafiscal_id_notafiscal;
 
