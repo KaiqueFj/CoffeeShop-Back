e03c6be95a2cd2dc0103165bed51cc79fb93658c
KaiqueFj <kaiquelferraz@hotmail.com>
2023-09-20 17:11:57 -0300

    feat: added correct error handling in FuncionarioController

--- src/main/java/com/example/CoffeeShop/controller/FuncionarioController.java
+++ src/main/java/com/example/CoffeeShop/controller/FuncionarioController.java
@@ -13,8 +13,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.CoffeeShop.model.cliente.Cliente;
 import com.example.CoffeeShop.model.funcionario.Funcionario;
 import com.example.CoffeeShop.repository.FuncionarioRepository;
+import com.example.CoffeeShop.service.Exception.CustomException;
 import com.example.CoffeeShop.service.FuncionarioDTO.FuncionarioReponseDTO;
 import com.example.CoffeeShop.service.FuncionarioDTO.FuncionarioRequestDTO;
 
@@ -32,7 +34,7 @@ public class FuncionarioController {
   // Add the employe in database
   @CrossOrigin(origins = "*", allowedHeaders = "*")
   @PostMapping(path = "/addFuncionario", consumes = "application/json;charset=UTF-8", headers = "content-type=text/json")
-  public ResponseEntity<Funcionario> saveFuncionario(@RequestBody FuncionarioRequestDTO data) {
+  public ResponseEntity<?> saveFuncionario(@RequestBody FuncionarioRequestDTO data) {
     try {
       Funcionario funcionarioData = new Funcionario(data);
       repository.save(funcionarioData);
@@ -40,14 +42,17 @@ public class FuncionarioController {
     }
 
     catch (Exception e) {
-      return new ResponseEntity<Funcionario>(HttpStatus.BAD_REQUEST);
+      String errorMessage = "Internal server error: " + e.getMessage();
+      CustomException errorResponse = new CustomException(errorMessage);
+      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
+
     }
   }
 
   // Get all employer
   @CrossOrigin(origins = "*", allowedHeaders = "*")
   @GetMapping("/getAllFuncionarios")
-  public ResponseEntity<List<FuncionarioReponseDTO>> getAll() {
+  public ResponseEntity<?> getAll() {
     try {
 
       List<FuncionarioReponseDTO> funcionarioList = repository.findAll().stream().map(FuncionarioReponseDTO::new)
@@ -57,7 +62,10 @@ public class FuncionarioController {
     }
 
     catch (Exception e) {
-      return new ResponseEntity<List<FuncionarioReponseDTO>>(HttpStatus.NOT_FOUND);
+      String errorMessage = "Internal server error: " + e.getMessage();
+      CustomException errorResponse = new CustomException(errorMessage);
+      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
+
     }
 
   }
@@ -65,40 +73,55 @@ public class FuncionarioController {
   // Get the employer by Id
   @CrossOrigin(origins = "*", allowedHeaders = "*")
   @GetMapping("/getEmployeebyId/{id_funcionario}")
-  public ResponseEntity<Funcionario> getBookById(@PathVariable Integer id_funcionario) {
-    Optional<Funcionario> funcionarioData = repository.findById(id_funcionario);
-    if (funcionarioData.isPresent()) {
-      return new ResponseEntity<>(funcionarioData.get(), HttpStatus.OK);
-    } else {
-      return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+  public ResponseEntity<?> getBookById(@PathVariable Integer id_funcionario) {
+    try {
+      Optional<Funcionario> funcionarioData = repository.findById(id_funcionario);
+      if (!funcionarioData.isPresent()) {
+        String errorMessage = "Não foi possível encontrar o cliente. Tente novamente !";
+        CustomException errorResponse = new CustomException(errorMessage);
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+
+      }
+      return ResponseEntity.status(HttpStatus.FOUND).body(funcionarioData);
+
+    }
+
+    catch (Exception e) {
+      String errorMessage = "Internal server error: " + e.getMessage();
+      CustomException errorResponse = new CustomException(errorMessage);
+      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
+
     }
   }
 
   // Delete the employer by Id
   @CrossOrigin(origins = "*", allowedHeaders = "*")
   @DeleteMapping("deleteFuncionarioByid/{id_funcionario}")
-  private ResponseEntity<String> deleteFornecedor(@PathVariable Integer id_funcionario) {
+  private ResponseEntity<?> deleteFornecedor(@PathVariable Integer id_funcionario) {
     try {
       boolean deleted = repository.existsById(id_funcionario);
 
       if (deleted) {
         repository.deleteById(id_funcionario);
-        return ResponseEntity.ok("Resouce Deleted successfully");
+        return ResponseEntity.ok("Funcionario deletado com sucesso!");
       } else {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Something went wrong, try again");
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+            .body("Erro ao tentar deletar funcionario, tente novamente");
       }
     }
 
     catch (Exception e) {
-      e.printStackTrace();
-      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Something went wrong, try again");
+      String errorMessage = "Internal server error: " + e.getMessage();
+      CustomException errorResponse = new CustomException(errorMessage);
+      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
+
     }
   }
 
   // Update the employe data
   @CrossOrigin(origins = "*", allowedHeaders = "*")
   @PutMapping("/updateFuncionario/{id_funcionario}")
-  private ResponseEntity<Funcionario> updateFuncionariobyId(@PathVariable Integer id_funcionario,
+  private ResponseEntity<?> updateFuncionariobyId(@PathVariable Integer id_funcionario,
       @RequestBody Funcionario newFuncionarioData) {
 
     try {
@@ -114,15 +137,17 @@ public class FuncionarioController {
         updateFuncionarioData.setT_equipe_id_equipe(newFuncionarioData.getT_equipe_id_equipe());
 
         repository.save(updateFuncionarioData);
-        return new ResponseEntity<Funcionario>(updateFuncionarioData, HttpStatus.OK);
+        return ResponseEntity.status(HttpStatus.OK).body(updateFuncionarioData);
       } else {
-        return new ResponseEntity<Funcionario>(HttpStatus.BAD_REQUEST);
+        String errorMessage = "Não foi possível atualizar os dados do cliente. Tente novamente !";
+        CustomException errorResponse = new CustomException(errorMessage);
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
       }
     }
 
     catch (Exception e) {
       e.printStackTrace();
-      return new ResponseEntity<Funcionario>(HttpStatus.BAD_REQUEST);
+      return new ResponseEntity<Cliente>(HttpStatus.NOT_MODIFIED);
     }
   }
 }
